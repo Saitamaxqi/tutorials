65e8d5aa5ccd0e9a3af408ea541cfc8c48c6e0a1
Saitamaxqi <Ahmedx39311@gmail.com>
2026-02-16 15:43:53 +0300

    added registration deadline and seats condition

--- training/models/training_registration.py
+++ training/models/training_registration.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import api, models, fields
+from odoo.exceptions import ValidationError
 
 class TrainingRegistration(models.Model):
     _name = 'training.registration'
@@ -16,3 +17,13 @@ class TrainingRegistration(models.Model):
     time = fields.Float(string='Time (hours)', related='course_id.time',readonly=True)
     room_name = fields.Char(string='Room Name', related='course_id.room_id.name',readonly=True)
     location_name = fields.Char(string='Location Name', related='course_id.location_id.name',readonly=True)
+
+    @api.model_create_multi
+    def create(self, vals):
+        for val in vals:
+            course = self.env['training.courses'].browse(val['course_id'])
+            if course.available_seats <= 0:
+                raise ValidationError("No available seats for this course.")
+            if course.deadline and fields.Date.today() > course.deadline:
+                raise ValidationError("Registration deadline has passed.")
+        return super(TrainingRegistration, self).create(vals)
